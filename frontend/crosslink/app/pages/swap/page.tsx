baf3859fd627ec04b65510d530d7ccaef8468ba4
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-09-11 13:53:21 +0300

    main page UI added & swap page imports added

--- frontend/crosslink/app/pages/swap/page.tsx
+++ frontend/crosslink/app/pages/swap/page.tsx
@@ -1,4 +1,19 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import { swap } from "../../../utils/functions/swapFunctions";
+import { getTokenInfo } from "../../../utils/functions/createTokenFunctions";
+import { decodeEventLog } from "viem";
+import { keccak256, toBytes } from "viem";
+import { config } from "../../../utils/config";
+import { PoolManagerABI } from "../../../utils/poolManagerABI.json";
+import { LiquidiytDeltaABI } from "../../../utils/readerABI.json";
+import { writeContract, readContract } from "@wagmi/core";
+import { ERC20ABI } from "../../../utils/ERC20ABI.json";
+import { waitForTransactionReceipt } from "@wagmi/core";
+import { useHook } from "../../../components/hookContext";
+import { getAllowance } from "../../../utils/functions/allowanceFunction";
+import { getBalance } from "../../../utils/functions/createTokenFunctions";
+
 
 const Swap = () => {
   return (
